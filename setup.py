b5da62937855b799de02cd823b4f03bf198f4204
Bridget Almas <balmas@gmail.com>
2016-09-08 09:15:47 -0400

    bump plokamos plugin version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='perseus_nemo_ui',
-    version="0.0.8",
+    version="0.0.9",
     packages=find_packages(exclude=["examples", "tests"]),
     url='https://github.com/PerseusDL/perseus_nemo_ui',
     license='GNU GPL',
@@ -11,7 +11,10 @@ setup(
     description='Plugin for Perseus UI for Nemo',
     test_suite="tests",
     install_requires=[
-        "flask_nemo>=1.0.0b1"
+        "flask_nemo>=1.0.0b1",
+        "nemo_arethusa_plugin>=0.0.1",
+        "nemo_oauth_plugin>=0.0.4",
+        "nemo_plokamos_plugin>=0.0.3"
     ],
     tests_require=[
         "capitains_nautilus>=0.0.5"

8393bdd1e8c50a9458683f6c62b7cf93f521f394
Bridget Almas <balmas@gmail.com>
2016-09-20 09:16:11 -0400

    add annotate in perseids link

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='perseus_nemo_ui',
-    version="0.0.11",
+    version="0.0.12",
     packages=find_packages(exclude=["examples", "tests"]),
     url='https://github.com/PerseusDL/perseus_nemo_ui',
     license='GNU GPL',

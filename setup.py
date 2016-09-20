afa3779c1d2995940f14fc44131e5f92432e72d6
Bridget Almas <balmas@gmail.com>
2016-09-20 16:04:09 -0400

    bump dependencies

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ setup(
     description='Plugin for Perseus UI for Nemo',
     test_suite="tests",
     install_requires=[
-        "flask_nemo>=1.0.0b1",
+        "flask_nemo>=1.0.0b3",
         "nemo_arethusa_plugin>=0.0.1",
         "nemo_oauth_plugin>=0.0.4",
         "nemo_plokamos_plugin>=0.0.4"
     ],
     tests_require=[
-        "capitains_nautilus>=0.0.5"
+        "capitains_nautilus>=0.0.6"
     ],
     include_package_data=True,
     zip_safe=False

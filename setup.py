ee27cd71f320298baac7e54d9c2fad0c435c8fb6
Bridget Almas <balmas@gmail.com>
2017-08-02 12:17:14 -0400

    bump release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='perseus_nemo_ui',
-    version="0.0.32",
+    version="0.0.33",
     packages=find_packages(exclude=["examples", "tests"]),
     url='https://github.com/PerseusDL/perseus_nemo_ui',
     license='GNU GPL',
